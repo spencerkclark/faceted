e9c76d419c69b62c9172c26dc4d7ceb1e877f381
Spencer Clark <spencerkclark@gmail.com>
2017-12-30 14:51:20 -0500

    Formalize package structure

---
+++ facets/tests/test_facets.py
@@ -0,0 +1,8 @@
+"""Test suite for facets module"""
+import numpy as np
+
+from facets import Tile
+
+
+def test_tile():
+    assert 1 == 1
---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+
+LONG_DESCRIPTION = """
+This is a module that I use in practice to produce both single and multi-panel
+figures for presentations and manuscripts. The reason I have gone through the
+trouble to write something like this is that I am particular about a few
+things:
+
+- I want tight, but easy, control over the space in between the panels of my
+figures in real space (not relative space).
+- I want tight control over the aspect ratio of the panels of my figure (e.g.
+when plotting maps), but still work within a strict dimensional constraint over
+the entire figure (e.g. I want to make a figure to fit in a column of a
+manuscript).
+- I want to make sure that the colorbars in all of my figures have the same
+thickness throughout my presentations or manuscripts; unfortunately it is hard
+to control this using the default matplotlib tools (you can set the thickness
+in relative space, but you need to be careful about what that means within the
+context of your figure size).
+"""
+
+
+setuptools.setup(
+    name='facets',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    author='Spencer K. Clark',
+    description='Precisely spaced subplots',
+    long_description=LONG_DESCRIPTION,
+    install_requires=[
+        'matplotlib >= 1.5',
+        'numpy >= 1.7',
+    ],
+    scripts=['facets/examples/Examples.ipynb']
+)
