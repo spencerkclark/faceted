5e0041aaad1cd843db9945a2d5f95452bace015b
spencerkclark <spencerkclark@gmail.com>
2021-12-31 16:20:13 -0500

    Restore Python 3.7 environment without cartopy

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering",
